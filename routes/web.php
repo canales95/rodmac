9d8a0c77a472f768a9ac9f927b556acaa9c1fc68
Fernando Canales <elfercanales@outlook.com>
2017-10-27 22:20:57 -0500

    env

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,36 @@ Route::match(['get','post'],'/', 'AdminController@viewIndex');
 Route::match(['get','post'],'login', 'AdminController@viewLogin');
 Route::match(['get','post'],'contenido', 'AdminController@viewContenido');
 // Route::match(['get','post'],'login', 'AdminController@viewLogin');
+
+APP_ENV=local
+APP_KEY=base64:OQHbqGXBJURqV9f2a1nc5sMYG8Rh+JhO/n3UPjv1PmY=
+APP_DEBUG=true
+APP_LOG_LEVEL=debug
+APP_URL=http://localhost
+
+DB_CONNECTION=mysql
+DB_HOST=127.0.0.1
+DB_PORT=3306
+DB_DATABASE=audisoft_db
+DB_USERNAME=root
+DB_PASSWORD=
+
+BROADCAST_DRIVER=log
+CACHE_DRIVER=file
+SESSION_DRIVER=file
+QUEUE_DRIVER=sync
+
+REDIS_HOST=127.0.0.1
+REDIS_PASSWORD=null
+REDIS_PORT=6379
+
+MAIL_DRIVER=smtp
+MAIL_HOST=mailtrap.io
+MAIL_PORT=2525
+MAIL_USERNAME=null
+MAIL_PASSWORD=null
+MAIL_ENCRYPTION=null
+
+PUSHER_APP_ID=
+PUSHER_KEY=
+PUSHER_SECRET=
