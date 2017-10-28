2db31b24da14f493e07137530307442936748da4
Fernando Canales <elfercanales@outlook.com>
2017-10-28 02:32:36 -0500

    resubiendo todo

---
+++ resources/views/rod-contenido.blade.php
@@ -0,0 +1,90 @@
+<!DOCTYPE html>
+<html lang="es">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Login</title>
+        <link rel="stylesheet" type="text/css" href="packages/bootstrap/css/bootstrap.min.css">
+          <link rel="stylesheet" type="text/css" href="packages/css/contenido.css">
+        <!--<link rel="stylesheet" type="text/css" href="mdb/css/bootstrap.min.css">-->
+      <!-- <link rel="stylesheet" type="text/css" href="mdb/css/mdb.min.css">-->
+  <link rel="stylesheet" type="text/css" href="libs/font-awesome-4.7.0/css/font-awesome.min.css">
+    </head>
+<body>
+  <div class="container">
+    <!-- Topper w/ logo -->
+    <div class="row hidden-xs topper">
+      <div class="col-xs-7 col-sm-7">
+        <a href="//convertify.io"><img am-TopLogo alt="SECUREVIEW"  src="http://blog.convertify.io/assets/images/logo--hero.png" class="img-responsive"></a>
+      </div>
+      <div class="col-xs-5 col-xs-offset-1 col-sm-5 col-sm-offset-0 text-right ">
+        <p am-CallNow>1 (888) CALL - NOW</p>
+      </div>
+    </div> <!-- End Topper -->
+    <!-- Navigation -->
+    <div class="row">
+      <nav class="navbar navbar-inverse" role="navigation">
+        <div class="container">
+          <!-- Brand and toggle get grouped for better mobile display -->
+          <div class="navbar-header">
+            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
+              <span class="sr-only">Toggle navigation</span>
+              <span class="icon-bar"></span>
+              <span class="icon-bar"></span>
+              <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand visible-xs-inline-block nav-logo" href="/"><img src="/images/logo-dark-inset.png" class="img-responsive" alt=""></a>
+          </div>
+
+          <!-- Collect the nav links, forms, and other content for toggling -->
+          <div class="collapse navbar-collapse navbar-ex1-collapse">
+            <ul class="nav navbar-nav js-nav-add-active-class">
+              <li><a href="/index.html">Home</a></li>
+              <li class="active"><a href="/markets.html">Markets</a></li>
+              <li><a href="/buy.html">Buy</a></li>
+              <li><a href="/svstrike.html">svStrike</a></li>
+              <li><a href="/training.html">Training</a></li>
+              <li><a href="/contact.html">Contact Us</a></li>
+              <li class="visible-xs-block"><a href="/about.html">About Us</a></li>
+              <li class="visible-xs-block"><a href="/careers.html">Careers</a></li>
+              <li class="visible-xs-block"><a href="/press-release.html">Press Release</a></li>
+              <li class="visible-xs-block"><a href="https://susteen7.zendesk.com/hc/en-us">Support</a></li>
+              <li class="dropdown">
+                <a href="#" class="dropdown-toggle" data-toggle="dropdown">Company <b class="caret"></b></a>
+                <ul class="dropdown-menu" role="menu">
+                  <li><a href="about.html">About Us</a></li>
+                  <li><a href="careers.html">Careers</a></li>
+                  <li><a href="press-release.html">Press Release</a></li>
+                </ul>
+              </li>
+            </ul>
+            <ul class="nav navbar-nav navbar-right hidden-xs">
+              <a type="button" class="navbar-btn btn btn-gradient-blue" am-latosans="bold" href="https://susteen7.zendesk.com/hc/en-us">Support</a>
+            </ul>
+          </div><!-- /.navbar-collapse -->
+        </div>
+      </nav>
+    </div>
+  </div>
+  <!--....................................................................-->
+<br>
+<br>
+<div class="container">
+	<div class="">
+    <div class="col-md-4">
+		<div class="form_main">
+                <h4 class="heading"><strong>Ingreso </strong> Imagenes <span></span></h4>
+                <div class="form-group">
+                <form action="contact_send_mail.php" method="post" id="contactFrm" name="contactFrm">
+<input type="file" name="archivo" /><br />
+                	 <textarea placeholder="Your Message" name="mess" type="text" class="txt_3">
+                	 </textarea>
+                     <input type="submit" value="submit" name="submit" class="txt2">
+                </form>
+            </div>
+            </div>
+            </div>
+	</div>
+</div>
+</body>
