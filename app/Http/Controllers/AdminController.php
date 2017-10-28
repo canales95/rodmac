d0c9335300083b29fb110c3983567c47d7c1daf4
Fernando Canales <elfercanales@outlook.com>
2017-10-27 19:32:01 -0500

    controlador de administrador

---
+++ app/Http/Controllers/AdminController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AdminController extends Controller
+{
+    public function viewIndex()
+    {
+      return view('rod-index');
+    }
+
+    public function FunctionName()
+    {
+      # code...
+    }
+}
